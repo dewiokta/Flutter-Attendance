1f6b6aa9a93d70de8772ed5f33731f23c177958c
Evina08 <evdinda08@gmail.com>
2022-02-11 00:11:19 +0700

    home page

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -19,13 +19,72 @@ class _HomePageState extends State<HomePage> {
         child: MainDrawer(),
       ),
       backgroundColor: Colors.white,
-      body: Container(
-        color: Colors.white,
-        child: Text(
-          "HomePage (bingung diisi apa hihi)",
-          style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
+      body: ListView(
+        padding: EdgeInsets.all(12),
+        physics: BouncingScrollPhysics(),
+        children: [
+          Container(height: 35),
+          userTile(),
+          divider(),
+          colorTiles(),
+          divider(),
+        ],
+      ),
+    );
+  }
+
+  Widget userTile() {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: AssetImage("assets/images/logo.png"),
+      ),
+      title: Text(
+        "Nama User",
+      ),
+      subtitle: Text(" Anggota "),
+    );
+  }
+
+  Widget divider() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Divider(
+        thickness: 1.5,
+      ),
+    );
+  }
+
+  Widget colorTiles() {
+    return Column(
+      children: [
+        colorTile(Icons.person_outline, Colors.deepPurple, "Profile"),
+        colorTile(Icons.settings_outlined, Colors.blue, "Presensi Hadir"),
+        colorTile(Icons.credit_card, Colors.pink, "Presensi Pulang"),
+        colorTile(Icons.favorite_border, Colors.orange, "Jadwal"),
+        colorTile(Icons.favorite_border, Colors.orange, "Riwayat Presensi"),
+        colorTile(Icons.favorite_border, Colors.orange, "Log Out"),
+      ],
+    );
+  }
+
+  Widget colorTile(IconData icon, Color color, String text,
+      {bool blackAndWhite = false}) {
+    Color pickedColor = Color(0xfff3f4fe);
+    return ListTile(
+      leading: Container(
+        child: Icon(icon, color: color),
+        height: 45,
+        width: 45,
+        decoration: BoxDecoration(
+          color: blackAndWhite ? pickedColor : color.withOpacity(0.09),
+          borderRadius: BorderRadius.circular(18),
         ),
       ),
+      title: Text(
+        text,
+      ),
+      trailing: Icon(Icons.arrow_forward_ios, color: Colors.black, size: 20),
+      onTap: () {},
     );
   }
 }
