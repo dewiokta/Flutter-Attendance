5a27db90c7594a0dbc5ca9ca8e10515c1806b953
Evina08 <evdinda08@gmail.com>
2022-02-28 13:47:37 +0700

    api login bismillah bisa

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_attendance/constants.dart';
 import 'package:flutter_attendance/pages/login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_attendance/constants.dart';
+import 'package:flutter_attendance/pages/login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_attendance/repository/auth_repository.dart';
+import 'package:flutter_attendance/state/auth_state.dart';
+import 'package:flutter_attendance/view/HomePage.dart';
+import 'blocs/Auth_bloc.dart';
+import 'event/auth_event.dart';
+import 'view/LoginPage.dart';
 
 void main() {
-  runApp(const MyApp());
+  final AuthRepository authRepository = AuthRepository();
+  runApp(BlocProvider(
+    create: (context) {
+      return AuthBloc(authRepository: authRepository);
+    },
+    child: MyApp(
+      authRepository: authRepository,
+      authBloc: AuthBloc(authRepository: authRepository),
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final AuthRepository authRepository;
+  final AuthBloc authBloc;
+
+  const MyApp({Key? key, required this.authRepository, required this.authBloc})
+      : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: kPrimaryColor, scaffoldBackgroundColor: Colors.white),
-      home: LoginScreen(),
+      home: BlocBuilder(
+        bloc: authBloc,
+        builder: (context, AuthState state) {
+          if (state is AuthInit) {
+            authBloc.add(AuthCheck());
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          if (state is AuthHasToken || state is AuthData)
+            return HomePage(
+              authBloc: authBloc,
+            );
+          if (state is AuthFailed || state is LoginFailed)
+            return LoginPage(authBloc: authBloc);
+          if (state is AuthLoading)
+            return Container(
+              color: Colors.white,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+
+          return Container();
+        },
+      ),
     );
   }
 }
---
+++ lib/view/LoginPage.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_attendance/blocs/Auth_bloc.dart';
+import 'package:flutter_attendance/event/auth_event.dart';
+import 'package:flutter_attendance/state/auth_state.dart';
+
+class LoginPage extends StatefulWidget {
+  final AuthBloc authBloc;
+
+  const LoginPage({Key? key, required this.authBloc}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  AuthBloc get _authBloc => widget.authBloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => _authBloc,
+      child: LoginForm(
+        authBloc: _authBloc,
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  final AuthBloc authBloc;
+
+  LoginForm({Key? key, required this.authBloc}) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final TextEditingController emailController = TextEditingController(text: "");
+
+  final TextEditingController passwordController =
+      TextEditingController(text: "");
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder(
+        bloc: widget.authBloc,
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text("Login Page"),
+            ),
+            body: SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(labelText: "Email"),
+                      ),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(labelText: "Password"),
+                      ),
+                      RaisedButton(
+                        onPressed: _login,
+                        child: (state is AuthLoading)
+                            ? Text("Please wait")
+                            : Text("Login"),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      (state is LoginFailed) ? Text(state.error) : Text(""),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  void _login() {
+    widget.authBloc.add(LoginProcess(
+      email: emailController.text,
+      password: passwordController.text,
+    ));
+  }
+}
